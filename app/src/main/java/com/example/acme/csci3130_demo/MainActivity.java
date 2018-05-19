c3bf3f374cc1cebb464d33fec4e113b64f4c177b
SyDu <siyidu95124@gmail.com>
2018-05-19 15:39:59 -0300

    Add the button and edittext

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private EditText editText;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button=(Button)findViewById(R.id.button);
+        editText=(EditText)findViewById(R.id.editText);
+        textView=(TextView)findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(editText.getText());
+
+            }
+        });
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
